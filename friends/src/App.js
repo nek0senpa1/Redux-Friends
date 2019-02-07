0891704d3e01a3185e0283d03150a8e22c7df222
amberm <nekosenpai@gmail.com>
2019-02-07 14:36:54 -0700

    initial setup running

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,23 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+//<img src={logo} className="App-logo" alt="logo" />
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
+          <h1>REDUX FRIENDS APP!</h1>
+
         </header>
       </div>
     );
---
+++ friends/src/store/reducers.js
@@ -0,0 +1,23 @@
+
+
+
+const initialStuff = {
+    
+    fetchingFriends: false,
+    friendsFetched: false,
+    friendsSaved: false,
+    savingFriends: false,
+    updatingFriend: false,
+    friendUpdated: false,
+    deletingFriend: false,
+    friendDeleted: false,
+    friends: [],
+    error: null
+      
+};
+
+const reducer = (state = initialStuff, action) => {
+    switch(action.type){}
+}
+
+export default reducer;
